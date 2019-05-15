704bcff7e01978b8dacccb0d80bbde5e3750b097
Justin Clift <justin@postgresql.org>
2019-05-15 14:18:33 +1000

    Initial copy of Ayke's portable gc code
    Just copied into the existing marksweep one for now, as it seems to
    be mostly a modification of that

---
+++ src/runtime/gc_stack_marksweep.go
@@ -0,0 +1,12 @@
+// +build gc.marksweep
+
+package runtime
+
+// markStack marks all root pointers found on the stack.
+//
+// This implementation is conservative and relies on the stack top (provided by
+// the linker) and getting the current stack pointer from a register. Also, it
+// assumes a descending stack. Thus, it is not very portable.
+func markStack() {
+	//markRoots(getCurrentStackPointer(), stackTop)
+}
